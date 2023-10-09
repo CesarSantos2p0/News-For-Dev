a65770f88b1d68d4908a18f6b4b684a8e8e60b0d
Paulo Cesar Santos Filho <111451739+CesarSantos2p0@users.noreply.github.com>
2023-10-09 16:28:34 +0000

    modificando a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>EU TE AMO MUITO MEU AMOR, VOCÊ É A MULHER DA MINHA VIDA!!!</h1>
+    return <h1>EU TE AMO MUITO MEU AMOR, VOCÊ É A MULHER DA MINHA VIDA TODINHA!!!</h1>
 }
 
 export default Home;

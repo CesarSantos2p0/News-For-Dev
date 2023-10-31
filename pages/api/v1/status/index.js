a2de848cd1ccf955090952a8142df7ff3ea4ee6a
Paulo Cesar Santos Filho <111451739+CesarSantos2p0@users.noreply.github.com>
2023-10-31 00:40:10 +0000

    add `.database` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1;");
+  console.log(result.rews);
   response.status(200).json({ chave: "são acima da media" });
 }
 

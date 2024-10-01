4ad11c1a9deea428208855edb43a2b23145fc3e2
Paulo Cesar Santos Filho <111451739+CesarSantos2p0@users.noreply.github.com>
2024-10-01 14:20:28 +0000

    add `jsconfig.json` file

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,8 @@
-import database from "../../../../infra/database.js";
+import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1;");
-  console.log(result.rews);
+  const result = await database.query("SELECT 1 + 1 as sum");
+  console.log(result.rows);
   response.status(200).json({ chave: "são acima da media" });
 }
 
